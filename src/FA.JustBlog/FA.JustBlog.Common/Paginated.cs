5d9f82b30500c59edf59e98fcf15de42bb1b60ca
Nguyen Khac Hoang <hoangncd3@gmail.com>
2021-08-09 14:36:59 +0700

    Add Common project

---
+++ src/FA.JustBlog/FA.JustBlog.Common/Paginated.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FA.JustBlog.Common
+{
+    public class Paginated<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+
+        public int TotalPages { get; private set; }
+
+        public bool HasPreviousPage => PageIndex > 1;
+
+        public bool HasNextPage => PageIndex < TotalPages;
+
+
+        public Paginated(List<T> entities, long count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            AddRange(entities);
+        }
+
+        public static async Task<Paginated<T>> CreateAsync(IQueryable<T> query, int pageIndex, int pageSize)
+        {
+            var count = await query.LongCountAsync();
+
+            var entities = await query.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new Paginated<T>(entities, count, pageIndex, pageSize);
+        }
+    }
+}
