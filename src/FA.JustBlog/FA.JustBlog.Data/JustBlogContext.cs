4e16c3937ceb1b7f303a6283917af900a785feee
Nguyen Khac Hoang <hoangncd3@gmail.com>
2021-08-09 13:47:40 +0700

    Add Data project

---
+++ src/FA.JustBlog/FA.JustBlog.Data/JustBlogContext.cs
@@ -0,0 +1,30 @@
+﻿using FA.JustBlog.Models.Common;
+using System.Data.Entity;
+
+namespace FA.JustBlog.Data
+{
+    public class JustBlogContext : DbContext
+    {
+        public JustBlogContext() : base("JustBlogConn")
+        {
+            Database.SetInitializer(new JustBlogInitializer());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>()
+                .HasMany(p => p.Tags)
+                .WithMany(t => t.Posts)
+                .Map(m =>
+                {
+                    m.MapLeftKey("PostId");
+                    m.MapRightKey("TagId");
+                    m.ToTable("PostTagMap");
+                });
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
--- src/FA.JustBlog/FA.JustBlog.Models/Common/Tag.cs
+++ src/FA.JustBlog/FA.JustBlog.Models/Common/Tag.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using FA.JustBlog.Models.BaseEntities;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace FA.JustBlog.Models.Common
 {
-    public class Tag
+    public class Tag : BaseEntity
     {
         [StringLength(255, MinimumLength = 2, ErrorMessage = "The {0} must between {2} and {1} character.")]
         [Required(ErrorMessage = "The {0} is required.")]
