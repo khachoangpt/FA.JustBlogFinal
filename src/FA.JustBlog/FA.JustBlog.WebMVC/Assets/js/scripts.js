8c4548297dc36caa9598f5cf09658c8fc9c4ba5a
Nguyen Khac Hoang <hoangncd3@gmail.com>
2021-08-09 23:06:28 +0700

    Fix bug and add images

---
+++ src/FA.JustBlog/FA.JustBlog.WebMVC/Assets/js/scripts.js
@@ -0,0 +1,5 @@
+﻿function convertToSlug() {
+    var title = document.getElementById('Title').value;
+    var result = title.toLowerCase().replace(/\s/g, '-').replace(/[^\w-]+/g, '');
+    document.getElementById('UrlSlug').value = result;
+}
